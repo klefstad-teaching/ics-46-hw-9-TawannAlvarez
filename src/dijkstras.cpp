9849f10dbf5d0c80a2d9f34f6fd2268861cf2543
TawannAlvarez <tawann@lazymonkey.net>
2025-03-16 23:15:06 +0000

    attemped dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,26 +1,42 @@
-constexpr int INF = std::numeric_limits<int>::max();
+#include "dijkstras.h"
 constexpr int UNDEFINED = -1;
-vector<int> dijkstra_shortest_path(Graph & graph, int source, vector<int>& previous){
-    int numVertices = graph.size();
+vector<int> dijkstra_shortest_path(const Graph & g, int source, vector<int>& previous){
+    int numVertices = g.numVertices;
     vector<int> distances(numVertices,INF);
     vector<bool> visited(numVertices, false);
     distances[source] = 0;
     previous[source] = UNDEFINED;
-    priority_queue<pair<int,int>> minHeap;
+    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
     minHeap.push({source,0});
     while(!minHeap.empty()){
-        int u = minHeap.extract_shortest_path().first;
+        int u = minHeap.top().first;
+        minHeap.pop();
         if(visited[u]) continue;
+
         visited[u] = true;
-        for(Edge edge:graph[u]){
+        for(Edge edge:g[u]){
             int v = edge.dest;
-            int weight = edge.second;
-            if(!visited && distances[u] + weight < distances[v]){
+            int weight = edge.weight;
+            if(!visited[v] && distances[u] + weight < distances[v]){
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap,push({v,distances[v]});
+                minHeap.push({v,distances[v]});
             }
         }
     }
     return distances;
+}
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+    vector<int> shortest_path;
+    for(int i = destination; i != -1; i = previous[i]){
+        shortest_path.insert(shortest_path.begin(), i);
+    }
+}
+void print_path(const vector<int>& v, int total){
+    cout << "Totoal cost:" << total < endl;
+    for(int i = 0; i < v.size(); i ++){
+        cout << v[i];
+    }
+    cout << endl;
+
 }
