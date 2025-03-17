08762f4cb968f08aab3638adc5d689ea468bf136
TawannAlvarez <tawann@lazymonkey.net>
2025-03-17 02:08:12 +0000

    fix print

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -89,7 +89,7 @@ void print_word_ladder(const vector<string>& ladder){
         cout << "No word ladder found." << endl;
         return;
     }
-    cout << "Word ladder found." << endl;
+    cout << "Word ladder found: ";
     for(const string& word : ladder){
         cout << word << ' ';
     }
